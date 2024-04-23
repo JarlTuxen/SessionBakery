fe5db285746de48a55170ce8d721687a1d74b696
jarltuxen <jarl.tuxen@gmail.com>
2024-04-23 16:05:44 +0200

    Initial commit

---
+++ src/main/java/dk/kea/sessionbakery/SessionBakeryApplication.java
@@ -0,0 +1,13 @@
+package dk.kea.sessionbakery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionBakeryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SessionBakeryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dk/kea/sessionbakery/SessionBakeryApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kea.sessionbakery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionBakeryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
